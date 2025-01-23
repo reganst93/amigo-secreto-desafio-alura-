6a708607fe9ad8fb7fbfa5236ea41d24c9a2efa9
regasnt93 <stephaniabilbao@gmail.com>
2025-01-23 19:09:43 -0300

    Primera tarea realizada

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo() {
+    //obtenemos lo ingresado por el usuario
+    let nombre = document.getElementById("amigo").value;
+    //aca verificamos si esta en blanco, si es asi se genera una alerta, si no se hace un push a la lista amigos.
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(nombre);
+    }
+    document.getElementById("amigo").value = "";
+}
